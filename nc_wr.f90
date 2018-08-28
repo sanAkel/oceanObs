a6c2c1cd3116111552d6a67baa2124a22a947441
sanAkel <18061653+sanAkel@users.noreply.github.com>
2018-08-28 18:09:16 -0400

    Delete nc_wr.f90

--- nc_wr.f90
+++
@@ -1,9 +0,0 @@
-http://www.oceandrivers.com/write-a-netcdfcf-file-with-python/
-
-https://cfpython.bitbucket.io/docs/0.9.4.2/build/write.html
-
-http://cedadocs.ceda.ac.uk/1048/1/03-creating_netcdf.pdf
-
-http://xarray.pydata.org/en/stable/io.html
-
-https://www.unidata.ucar.edu/software/netcdf/examples/programs/example_h3_cf1_6.f90
